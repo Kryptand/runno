97caff0a8f2fc93499e07c196ffb2dea52baf7b9
Kryptand <goetze-a@outlook.com>
2021-09-01 07:52:35 +0200

    kram

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,11 +12,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.enableCors({
-    "origin": true,
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
-    "credentials":true
+    origin: [/^(.*)/],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders:
+      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
   });
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
